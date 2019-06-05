7a65567fd2109a199c51f069aa219aeab9bd6cb0
Jorge Barrón <jorgeb8168711@gmail.com>
2019-06-05 15:32:46 -0500

    Updated project for deploy to github pages

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/angular-ngrx-income-expenses-app'),
+      dir: require('path').join(__dirname, '../coverage/ngrx-income-expenses-app'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
